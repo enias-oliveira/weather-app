8b38dfaa0ce93a683e0876f9ce681d8107dc1bc3
enias-oliveira <enias.felipe@gmail.com>
2024-08-24 13:00:11 -0300

    chore: adds prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
-import { useApp } from "./use-app";
-import type { ResponseItem } from "./api";
-import React from "react";
-import { WeatherPanel } from "./WeahterPanel";
-import { CapitalsList } from "./CapitalsList";
+import { useState } from 'react'
+import { useApp } from './use-app'
+import type { ResponseItem } from './api'
+import React from 'react'
+import { WeatherPanel } from './WeahterPanel'
+import { CapitalsList } from './CapitalsList'
 
 function App() {
-  const { capitalsWeather } = useApp();
+    const { capitalsWeather } = useApp()
 
-  const [selectedCapital, setSelectedCapital] = useState<ResponseItem>();
+    const [selectedCapital, setSelectedCapital] = useState<ResponseItem>()
 
-  const handleClickCapitalListItem = (cw: ResponseItem) => () =>
-    setSelectedCapital(cw);
+    const handleClickCapitalListItem = (cw: ResponseItem) => () =>
+        setSelectedCapital(cw)
 
-  return (
-    <>
-      <WeatherPanel capital={selectedCapital} />
-      <CapitalsList  capitals={capitalsWeather}  handleClickOnCapital={handleClickCapitalListItem} />
-    </>
-  );
+    return (
+        <>
+            <h1>Previsão do Tempo</h1>
+            <WeatherPanel capital={selectedCapital} />
+            <CapitalsList
+                capitals={capitalsWeather}
+                handleClickOnCapital={handleClickCapitalListItem}
+            />
+        </>
+    )
 }
 
-export default App;
+export default App
