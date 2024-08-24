d20ed60f2958ef907b79a9d1176aed6172e87ef9
enias-oliveira <enias.felipe@gmail.com>
2024-08-24 13:36:00 -0300

    feat(wip): add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,52 @@ import type { ResponseItem } from './api'
 import React from 'react'
 import { WeatherPanel } from './WeahterPanel'
 import { CapitalsList } from './CapitalsList'
+import { FaSearch } from 'react-icons/fa'
 
 function App() {
     const { capitalsWeather } = useApp()
 
+    const [searchInputText, setSearchInputText] = useState('')
+    const [searchValue, setSearchValue] = useState('')
     const [selectedCapital, setSelectedCapital] = useState<ResponseItem>()
 
     const handleClickCapitalListItem = (cw: ResponseItem) => () =>
         setSelectedCapital(cw)
 
+    const handleSearch = () => {
+        setSelectedCapital(undefined)
+        setSearchInputText('')
+        setSearchValue(searchInputText)
+    }
+
+    const searchRegex = new RegExp(searchValue, 'g')
+
+    const filteredCapital =
+        selectedCapital ??
+        capitalsWeather.find(({ location: { city } }) => searchRegex.test(city))
+
     return (
         <>
             <h1>Previsão do Tempo</h1>
-            <WeatherPanel capital={selectedCapital} />
+            <WeatherPanel capital={filteredCapital} />
+
+            <div>
+                <input
+                    type="text"
+                    placeholder="Insira aqui o nome da cidade"
+                    value={searchInputText}
+                    onChange={(e) => setSearchInputText(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSearch()
+                        }
+                    }}
+                />
+                <button onClick={handleSearch}>
+                    <FaSearch />
+                </button>
+            </div>
+
             <CapitalsList
                 capitals={capitalsWeather}
                 handleClickOnCapital={handleClickCapitalListItem}
--- src/CapitalsList.tsx
+++ src/CapitalsList.tsx
@@ -1,22 +1,28 @@
-import React, { FC } from "react";
-import { ResponseItem } from "./api";
+import React, { FC } from 'react'
+import { ResponseItem } from './api'
 
 type CapitalSelectionsProps = {
-    capitals?: ResponseItem[],
+    capitals?: ResponseItem[]
     handleClickOnCapital: (captial: ResponseItem) => () => void
 }
 
 export const CapitalsList: FC<CapitalSelectionsProps> = ({
-   capitals,
-   handleClickOnCapital
+    capitals,
+    handleClickOnCapital,
 }) => {
-    return ( 
-      <ul>
-        {capitals?.map((cpt) => (
-          <li key={cpt.location.city} onClick={handleClickOnCapital(cpt)}>
-            {cpt.location.city}
-          </li>
-        ))}
-      </ul>
-     );
+    return (
+        <div>
+            <h2>Capitais</h2>
+            <ul>
+                {capitals?.map((cpt) => (
+                    <li
+                        key={cpt.location.city}
+                        onClick={handleClickOnCapital(cpt)}
+                    >
+                        {cpt.location.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
